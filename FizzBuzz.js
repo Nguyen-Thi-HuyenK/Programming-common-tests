8718f8ebba990a4a4d4010abcb3968a42886c959
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-11-24 15:34:12 +0200

    second option

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -14,3 +14,23 @@ let fizzBuzz = function(n) {
 };
 
 fizzBuzz(15);
+
+// second option
+let output = [];
+
+function fizzBuzz2() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      output.push("Fizz");
+    } else if (i % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(i);
+    }
+  }
+console.log(output);
+}
+
+fizzBuzz2();
